61f74db0a40e4c70603659686227fb376aafb86f
agjpTdptgm <168077576+agjpTdptgm@users.noreply.github.com>
2025-06-27 12:18:10 +0900

    Dice game:initial revision

---
+++ .c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    
+srand(time(NULL));  
+
+int n1 = rand() % 6 +1;
+int n2 = rand() % 6 +1;
+int i=0;
+printf("Rolling dice...");
+printf("Die %d: %d",++i,n1);
+printf("Die %d: %d",++i,n2);
+printf("Total value: %d",n1+n2);
+
+  
+}
